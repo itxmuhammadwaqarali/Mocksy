02c4acad72836bb39016124fe80c7aa1f1b9f6aa
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-09 15:30:48 +0500

    add get_cv_by_id function to crud/cv.py

--- app/crud/cv.py
+++ app/crud/cv.py
@@ -1,9 +1,14 @@
 from sqlalchemy.orm import Session
 from app.models.cv import CV
 
+
 def create_cv(db: Session, user_id: int, file_path: str, extracted_data: dict):
     cv = CV(user_id=user_id, file_path=file_path, extracted_data=extracted_data)
     db.add(cv)
     db.commit()
     db.refresh(cv)
     return cv
+
+
+def get_cv_by_id(db: Session, cv_id: int):
+    return db.query(CV).filter(CV.id == cv_id).first()
