5e4295a783c808e608a18d38c1bbdde2e29b3311
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-13 14:58:59 +0500

    add function to fetch CVs by user ID in cv.py

--- app/crud/cv.py
+++ app/crud/cv.py
@@ -12,3 +12,7 @@ def create_cv(db: Session, user_id: int, file_path: str, extracted_data: dict):
 
 def get_cv_by_id(db: Session, cv_id: int):
     return db.query(CV).filter(CV.id == cv_id).first()
+
+
+def get_user_cvs(db: Session, user_id: int):
+    return db.query(CV).filter(CV.user_id == user_id).all()
