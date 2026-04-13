827a679969802f27d836056f333b86e9d517b623
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-13 16:04:15 +0500

    update CORS settings to allow all origins and disable credentials

--- app/main.py
+++ app/main.py
@@ -16,13 +16,8 @@ app = FastAPI()
 # ✅ Add CORS middleware for React
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
