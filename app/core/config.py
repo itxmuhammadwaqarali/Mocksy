972ab252511c14fdea5ae9f7d99d93fb44a78555
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-14 11:54:47 +0500

    load SECRET_KEY only from environment variables for improved security

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:123@localhost:5432/mocksy")
 
 # JWT
-SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
 # App
